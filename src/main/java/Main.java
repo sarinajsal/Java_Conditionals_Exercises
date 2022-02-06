2fea6ebf9582c4785ffeee0a9e8915745736ea15
Sarina Salamon <salamon.sarina@gmail.com>
2022-02-06 18:41:03 +0000

    up to exercise7

---
+++ src/main/java/Main.java
@@ -0,0 +1,74 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // create an integer variable
+        // create a second integer variable
+        // create an `if` statement which compares the two variables and prints to the console if they are equal
+
+        int numOne = 1;
+        int numTwo = 1;
+        if (numOne == numTwo) {
+            System.out.println(numOne + " " + numTwo);
+
+        }
+
+        // create a string variable
+        // create a second string variable which has the same value as the first
+        // create an `if` statement which compares the two variables and prints to the console if they are equal
+        String strOne = "Hello";
+        String strTwo = "Hello";
+        if (strOne.equals(strTwo)) ;
+        System.out.println(strOne + " " + strTwo);
+
+        // create an string variable using the `new` keyword
+        // create a second string variable which has the same value as the first, again using the `new` keyword
+        // create an `if` statement which compares the two variables and prints to the console if they are equal
+
+        String string = new String("hi");
+        String stringTwo = new String("hi");
+        if (string.equals(stringTwo) ){
+            System.out.println("string is " + string + " stringTwo is also " + stringTwo);
+        }
+
+        // create two `int` variables. Assign them values
+        // create an `if` statement which compares whether one value is greater than the other
+        // print something to the console if the condition is met
+        // create an `else` statement which prints to the console if the above condition is NOT met
+
+        int n1 = 1;
+        int n2 = 1;
+        if (n1 == n2){
+            System.out.println("n1 and n2 are equal");
+        }
+        else {
+            System.out.println("n1 and n2 are not equal");
+        }
+        // create three `int` variables. Assign them values with the first value sitting between the other two
+        // create an `if` statement which compares whether the first value is greater than the second AND less than the third
+        // print something to the console if the condition is met
+        // create an `else` statement which prints to the console if the above condition is NOT met
+
+        //Modify your answer to Question 5 to instead use an OR operator. Play about with the values assigned to your three variables, noting how the behaviour changes.
+
+        int num1 = 10;
+        int num2 = 11;
+        int num3 = 9;
+        if (num1>num2 || num1<num2){
+            System.out.println("condition met");
+        }
+        else {
+            System.out.println("condition not met");
+        }
+        // create a String variable and assign it a value
+        // create an `if` statement which grabs the first letter of your word and compares it against a Character value
+        // print something to the console if the condition is met
+
+        String myString = "Hello world";
+        char h = 'h';
+        if (Character.toLowerCase(myString.charAt(0)) == h);
+        System.out.println("condition met");
+
+    }
+    
+}
+
